0c2bffe1c5e85fc24823e71a804e21ba8e410838
WansGoh <danielgo512@gmail.com>
2019-07-31 08:56:27 -0700

    first commit

---
+++ functions.hpp
@@ -0,0 +1,26 @@
+//
+//  functions.hpp
+//  main
+//
+//  Created by Daniel Yuwan Goh on 2019-07-31.
+//  Copyright © 2019 Yuwan Go. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+
+
+//prototype
+
+void someDataTypes(void);
+void stackMenu();
+bool push(int val);
+void pop();
+void printStack();
+void message (std::string msg);
+
+void InitStack();
+bool PushWithPointer(int);
+int PopWithPointer();
+void PrintStackWithPointer();
+void stackMenuWithPointer();
---
+++ main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  main
+//
+//  Created by Daniel Yuwan Goh on 2019-07-31.
+//  Copyright © 2019 Yuwan Go. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+#include "functions.hpp"
+
+
+
+int main(){
+    
+    stackMenuWithPointer();
+    
+    return 0;
+}
